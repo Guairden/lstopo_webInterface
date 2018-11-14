c94543ca62ef059e94febf5b3c1f802d2e7a9146
Valentin Hoyet <valentin.hoyet@inria.fr>
2018-11-14 17:01:57 +0100

    Revert "fusion : bulleInfo -> main <- hide element"
    This reverts commit 7f9c9e3b59f7ddfd168b78557d49d17f12c567f4.

--- Interface_web_lstopoV9/js/L2Cache.js
+++ Interface_web_lstopoV9/js/L2Cache.js
@@ -4,4 +4,61 @@ class L2Cache extends Node{
         this.type = "L2Cache";
         //this.weight = '(' + path["@attributes"]["cache_size"].slice(-0,-3) + 'KB)';
     }
+    createElement(){
+        const div = document.createElement('div');
+        const P = document.createElement('p');
+
+        P.innerHTML = this.type + this.weight;
+        div.appendChild(P);
+        div.classList.add("L2");
+        this.parentElement.appendChild(div);
+        return div
+    }
+
+    createInfoPopup(){
+        const modal = document.createElement('div')
+        modal.id = "myModal"
+        modal.classList.add("modal")
+        
+        const modalContent = document.createElement('div')
+        modalContent.classList.add("modal-content")
+
+        const modalHeader = document.createElement('div')
+        modalHeader.classList.add("modal-header")
+
+        const h2 = document.createElement('h2')
+        h2.innerHTML = "modalHeader"
+
+        const modalBody = document.createElement('div')
+        modalBody.classList.add("modal-body")
+
+        const p1 = document.createElement('p')
+        p1.innerHTML = this.path["@attributes"].gp_index
+
+        const p2 = document.createElement('p')
+        p2.innerHTML = "Some other text..."
+
+        modalBody.appendChild(p1)
+        modalBody.appendChild(p1)
+
+        modalHeader.appendChild(h2)
+
+        modalContent.appendChild(modalHeader)
+        modalContent.appendChild(modalBody)
+
+        modal.appendChild(modalContent)
+        document.body.appendChild(modal)
+
+        this.div.addEventListener('click', function() {
+            modal.style.display = "block";
+        })
+        
+        
+        // When the user clicks anywhere outside of the modal, close it
+        window.addEventListener('click', function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        })
+    }
 }
--- Interface_web_lstopoV9/js/Node.js
+++ Interface_web_lstopoV9/js/Node.js
@@ -9,7 +9,7 @@ class Node{
             this.package = nodeInfo.package
             this.parent = this.findParent(nodeInfo)
             this.div = this.createDiv()
-            this.createInfoPopup()
+            create
         }  
     }
 
@@ -20,76 +20,6 @@ class Node{
         node.appendChild(p)
         node.classList.add(this.path["@attributes"].type)
         return node;
-    }
-
-    createInfoPopup(){
-        
-        const modal = document.createElement('div')
-        modal.id = "myModal"
-        modal.classList.add("modal")
-        
-        const modalContent = document.createElement('div')
-        modalContent.classList.add("modal-content")
-
-        const modalHeader = document.createElement('div')
-        modalHeader.classList.add("modal-header")
-
-        const h2 = document.createElement('h2')
-        h2.innerHTML = this.path["@attributes"].type
-
-        const modalBody = document.createElement('div')
-        modalBody.classList.add("modal-body")
-
-        const p1 = document.createElement('p')
-        p1.innerHTML = "gp_index : " + this.path["@attributes"].gp_index
-
-        
-        const p2 = document.createElement('p')
-        p2.innerHTML = "complete_cpuset :" + this.path["@attributes"].complete_cpuset
-
-        const p3 = document.createElement('p')
-        p3.innerHTML = "complete_nodeset :" + this.path["@attributes"].complete_nodeset
-
-        const p4 = document.createElement('p')
-        const p5 = document.createElement('p')
-
-        if(this.path["@attributes"].cache_associativity){
-            p4.innerHTML = "cache_associativity :" + this.path["@attributes"].cache_associativity
-            p5.innerHTML = "cache_type :" + this.path["@attributes"].cache_type    
-        }else{
-            p4.innerHTML = "os_index :" + this.path["@attributes"].os_index
-        }
-        
-
-        modalBody.appendChild(p1)
-        modalBody.appendChild(p2)
-        modalBody.appendChild(p3)
-        modalBody.appendChild(p4)
-        modalBody.appendChild(p5)
-        
-        modalHeader.appendChild(h2)
-
-        modalContent.appendChild(modalHeader)
-        modalContent.appendChild(modalBody)
-
-        modal.appendChild(modalContent)
-        document.body.appendChild(modal)
-
-        this.div.addEventListener('click', function(e) {
-            //a little complex if that solve this problem : when you click on PU, Core popup event get triggered (PU being inside the core div..)
-            if((e.currentTarget != e.target && e.target.tagName != ('P')) ||
-                (e.currentTarget.classList.contains('Core') && e.target.tagName == 'P' && e.target.parentElement.classList.contains('PU') ))
-                return;
-            modal.style.display = "block";
-        })
-        
-        
-        // When the user clicks anywhere outside of the modal, close it
-        window.addEventListener('click', function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        })
     }
 
     findParent(nodeInfo){
